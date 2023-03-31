bfcfa6aabdb1778d6308efeeab0790a9ec27a84a
wojciech krupa <wojciech.w.krupa@gmail.com>
2023-03-31 20:14:19 +0100

    updated styling, added airplane component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import MySkills from "./scenes/MySkills";
 import Projects from "./scenes/Projects";
 import Contact from "./scenes/Contact";
 import Footer from "./scenes/Footer";
+import Airplane from "./scenes/Airplane";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState("home");
@@ -32,34 +33,40 @@ function App() {
   }, []);
 
   return (
-    <div className="app bg-deep-blue">
+    <div className="app bg-deep-blue relative">
       <Navbar
         isTopOfPage={isTopOfPage}
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
       />
-      <div className="w-5/6 mx-auto md:min-h-full">
-        {isAboveMediumScreens && (
-          <DotGroup
-            selectedPage={selectedPage}
-            setSelectedPage={setSelectedPage}
-          />
-        )}
-        <Landing setSelectedPage={setSelectedPage} />
+      <div className="bg-pattern-landing">
+        <div className="w-5/6 mx-auto md:min-h-full">
+          {isAboveMediumScreens && (
+            <DotGroup
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+          )}
+          <Landing setSelectedPage={setSelectedPage} />
+        </div>
       </div>
       <LineGradient />
       <div className="w-5/6 mx-auto md:min-h-full">
         <MySkills setSelectedPage={setSelectedPage} />
       </div>
       <LineGradient />
-      <div className="w-5/6 mx-auto">
-        <Projects setSelectedPage={setSelectedPage} />
+      <div className="bg-pattern-projects">
+        <div className="w-5/6 mx-auto">
+          <Projects setSelectedPage={setSelectedPage} />
+        </div>
       </div>
       <LineGradient />
       <div className="w-5/6 mx-auto">
         <Contact setSelectedPage={setSelectedPage} />
       </div>
-      <Footer />
+
+      <Airplane height={"140px"} />
+      <Footer className="z-10" />
     </div>
   );
 }
