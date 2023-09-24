ec353496a9dc263ee3045e798a06b4b01551316e
wojciech krupa <wojciech.w.krupa@gmail.com>
2023-09-24 22:49:49 +0100

    fix: Fix Bug - Replace WK Logo <a> with <AnchorLink>

--- src/scenes/Footer.jsx
+++ src/scenes/Footer.jsx
@@ -1,13 +1,22 @@
+import AnchorLink from "react-anchor-link-smooth-scroll";
 import SocialMediaIcons from "../components/SocialMediaIcons";
-const Footer = () => {
+
+const Footer = ({ setSelectedPage }) => {
   return (
     <footer className="h-64 bg-primary-2 bg-gradient-1 pt-10">
       <div className="w-5/6 mx-auto">
         <SocialMediaIcons color="dark-1" />
         <div className="md:flex justify-center md:justify-between text-center font-semibold ">
-          <a href="/#home" className="font-playfair text-2xl text-dark-1">
+          {/* <a href="#home" className="font-playfair text-2xl text-dark-1">
             WK{" "}
-          </a>
+          </a> */}
+          <AnchorLink
+            className="font-playfair text-2xl text-dark-1"
+            href="#home"
+            onClick={() => setSelectedPage("home")}
+          >
+            WK
+          </AnchorLink>
           <p className="text-md text-dark-1">
             &copy;2023 WOJCIECH KRUPA. All Rights Reserved.
           </p>
