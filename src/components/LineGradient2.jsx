dafab5c0923b2bf595719809cc809bcab249dfc7
wojciech krupa <wojciech.w.krupa@gmail.com>
2023-11-09 16:15:42 +0000

    Added info page with redirection for an updated page.

--- src/components/LineGradient2.jsx
+++
@@ -1,5 +0,0 @@
-const LineGradient = ({ width = "w-full" }) => {
-  return <div className={`h-0.5 ${width} bg-gradient-2`}></div>;
-};
-
-export default LineGradient;
--- src/components/LineGradient3.jsx
+++
@@ -1,5 +0,0 @@
-const LineGradient = ({ width = "w-full" }) => {
-  return <div className={`h-0.5 ${width} bg-gradient-3`}></div>;
-};
-
-export default LineGradient;
